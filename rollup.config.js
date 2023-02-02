d42eebd29d09e244da38574ab1a9db68c7779124
Mahmoud Kozae <mahmoud.kozae@outlook.com>
2023-02-02 11:11:35 +0100

    renamed folder from react-src to playground-src

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
-    input: "react-src/index.ts",
+    input: "playground-src/index.ts",
     output: [
       {
         file: packageJson.main,
